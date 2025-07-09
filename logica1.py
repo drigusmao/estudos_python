90f4e263498048694eadebd1283ab0d27e62fc03
Adriana Gusmão <adriana.gusmao@outlook.com>
2025-07-09 19:26:34 -0300

    Atualização 09/07/2025

--- logica1.py
+++ logica1.py
@@ -70,7 +70,126 @@ if entrada == 'entrar' and senha_digitada == senha_usuario: #Só irá rodar o pr
 else:
      print('Saída do programa.')
 
+"""
+or - Se uma condição for verdadeira considera toda a expressão como verdadeira.
+Se qualquer valor for considerado verdadeiro, a expressão inteira será avaliada naquele valor.
 
+"""
+
+#Vamos continuar a usar as variaveis do exercicio anterior para exempleficar o uso do 'or'
+#Vamos supor que o usuario não escreveu a palavra entrar toda em minuscula mas usou a primeira letra maiuscula isso não pode impedi-lo de acessar o programa.
+
+if (entrada == 'entrar' or 'Entrar') and senha_digitada == senha_usuario: #Só irá rodar o programa se uma das condições 'or' for True.
+   print('Entrar')
+else:
+   print('Sair')
+
+"""
+not - Inverte expressões, o que for True vira False e o que for False vira True.
+
+Operadores in e not in
+
+Strings em Python são iteráveis (você pode navegar item por item usando seus indices ex Adriana pode navegar assim: A d r i a n a):
+0 1 2 3 4 5
+O t á v i o
+-6-5-4-3-2-1
+
+"""
+
+nome = 'Adriana'
+
+#Vamos acessar a letra i que está no indice 3 (começamos a contar do 0 ou seja a quantidade de elementos -1)
+print(nome[3])
+
+#Vamos ver se um x elemento está dentro da palavra Adriana
+#A letra y está no nome Adriana? Se tiver ira aparecer True, se não aparecerá False.
+print('y' in nome)
+
+#Faça um programa que peça o nome do usuário e o que ele quer checar dentro do nome dele:
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
+
+"""
+Interpolação básica de strings (format de um jeito diferente)
+s = string
+d e i = int
+f = float
+x e X = Hexadecimal (ABCDEF0123456789) ('numeros' formados de letras do A ao E e numeros do 0 a 9)
+"""
+
+nome = 'Adriana'
+preco = 25
+variavel = 'Adriana, o preço total foi R$25.'
+variavel_interpolaçao = '%s, o preço total foi R$%s' % (nome, preco) #Interpolação feita (coloco o %+s e depois coloco , e identifico na ordem o que é cada parte da interpolação).
+print(variavel_interpolaçao)
+
+"""
+Formatação básica de strings.
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o número a aparecer antes dos zeros.
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a 
+"""
+
+#Exemplos
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}') #Fez uma largura fixa (preenche com espaços com 10 caracteres para esquerda alem do ABC que são 3 vai mais 7 para a esquerda).
+print(f'{variavel: <10}.') #Largura fixa para direita
+print(f'{variavel: ^10}.') #Faz largura fixa divindo na metade
+
+"""
+Fatiamento de strings (pega uma 'fatia' uma parte da sua 'string')
+
+ Olá mundo
+ 012345678 #Posições na string acima
+-987654321 #Negativos (do final para o começo)
+
+"""
+
+#Onde quero o fatimamento da string?
+variavel = 'Olá mundo'
+print(variavel[4:8]) #Coloco o começo do fatiamento  ':'  até o final +1 pq exclui um do indice final
+#Se coloco :7 vai do começo até o indice 6
+#Se coloco 3: vai do indice 3 até o final
+
+print(len(variavel)) #A função len conta a quantidade de caracteres das strings
+
+print(variavel[0:9:3]) #Lerá cada parte citada
+
+#Exercicio: Pela ao usuario seu nome, sua idade e se forem digitados exiba o {nome}, (nome invertido), se tem ou não espaços, quantidade de letras, primeira letra do nome, a ultima e se nada for digitado em nome e idade exiba a mensagem "Desculpe, voce deixou campos vazios".
+
+nome = (input('Digite seu nome: '))
+idade = (input('Digite sua idade '))
+
+if nome and idade:
+  print(f'Seu nome é {nome}.')
+  print(f'Seu nome invertido é {nome[::-1]}.')
+  print(f'Seu nome tem {len(nome)} letras.')
+  print(f'A primeira letra do seu nome é {nome[0]}.')
+  print(f'A ultima letra do seu nome é {nome[-1]}.')
+  if ' ' in nome:
+    print('Seu nome tem espaços.')
+  else:
+    print('Seu nome não tem espaços.')
+else:
+  print('Desculpe, você deixou campos vazios.')
+  
 
 
 
